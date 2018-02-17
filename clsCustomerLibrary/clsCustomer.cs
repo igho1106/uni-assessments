b2fe663b80cb9685e8ac61ae50d3ba4d158c7c91
ighoakponah <ighoakponah@IGHOAKPONAH7872>
2018-02-17 15:11:40 +0000

    Add project files.

---
+++ clsCustomerLibrary/clsCustomer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+
+
+namespace clsCustomerLibrary
+{
+    public class clsCustomer
+    {
+        //private data member for the CustomerID property
+        private int mCustomerID;
+        //private data member for the DOB property
+        private DateTime mDOB;
+        //private data member for the EmailAddress property
+        private string mEmailAddress;
+        //private data member for the FirstName property
+        private string mFirstName;
+        //private data member for the Gender property
+        private string mGender;
+        //private data member for the HomeAddress property
+        private string mHomeAddress;
+        //private data member for the LastName property
+        private string mLastName;
+        //private data member for the PhoneNo property
+        private string mPhoneNo;
+        //private data member for the Active property
+        private Boolean mActive;
+
+
+        public bool Active
+        {
+            get
+            {
+                //return the private data
+                return mActive;
+            }
+            set
+            {
+                //set the value of the private data member
+                mActive = value;
+            }
+        }
+        public int CustomerID
+        {
+            get
+            {
+                //return the private data
+                return mCustomerID;
+            }
+            set
+            {
+                //set the value of the private data member
+                mCustomerID = value;
+            }
+        }
+        public DateTime DOB
+        {
+            get
+            {
+                //return the private data
+                return mDOB;
+            }
+            set
+            {
+                //set the value of the private data member
+                mDOB = value;
+            }
+        }
+        public string EmailAddress
+        {
+            get
+            {
+                //return the private data
+                return mEmailAddress;
+            }
+            set
+            {
+                //set the value of the private data member
+                mEmailAddress = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                //return the private data
+                return mFirstName;
+            }
+            set
+            {
+                //set the value of the private data member
+                mFirstName = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                //return the private data
+                return mGender;
+            }
+            set
+            {
+                //set the value of the private data member
+                mGender = value;
+            }
+        }
+
+        public string HomeAddress
+        {
+            get
+            {
+                //return the private data
+                return mHomeAddress;
+            }
+            set
+            {
+                //set the value of the private data member
+                mHomeAddress = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                //return the private data
+                return mLastName;
+            }
+            set
+            {
+                //set the value of the private data member
+                mLastName = value;
+            }
+        }
+        public string PhoneNo
+        {
+            get
+            {
+                //return the private data
+                return mPhoneNo;
+            }
+            set
+            {
+                //set the value of the private data member
+                mPhoneNo = value;
+            }
+        }
+        public bool Find(int customerID)
+        {
+            //set the private data members to the test data value
+            
+            mHomeAddress = "6 Cavendish Mews";
+            mPhoneNo = "0123456789";
+            mGender = "male";
+            mLastName = "akponah";
+            mFirstName = "igho";
+            mEmailAddress = "igho@yahoo.com";
+            mDOB = Convert.ToDateTime("11/06/1989");
+            mActive = true;
+            //always return true
+            return true;
+        }
+
+        
+        public string Valid(string firstName, string lastName, string gender, string homeAddress, string emailAddress, string phoneNo, string DOB)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            //if the FirstName is blank
+            if (firstName.Length == 0)
+            {
+                //record the error
+                Error = Error + "The FirstName field must not be blank: ";
+            }
+            //return any error messages
+            return Error;
+        }
+    }
+}
---
+++ tstCustomer/tstCustomer.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using clsCustomerLibrary;
+
+namespace tstCustomer
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            //test to ensure it exists
+            Assert.IsNotNull(ACustomer);
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            Boolean TestData = true;
+            //assign data to the property
+            ACustomer.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.Active, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIDOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            int TestData = 1;
+            //assign data to the property
+            ACustomer.CustomerID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void DOBPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign data to the property
+            ACustomer.DOB = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.DOB, TestData);
+        }
+
+        [TestMethod]
+        public void EmailAddressPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            string TestData = "abc1@yahoo.com";
+            //assign data to the property
+            ACustomer.EmailAddress = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.EmailAddress, TestData);
+        }
+
+        [TestMethod]
+        public void FirstNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            string TestData = "John";
+            //assign data to the property
+            ACustomer.FirstName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.FirstName, TestData);
+        }
+
+        [TestMethod]
+        public void GenderPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            string TestData = "M";
+            //assign data to the property
+            ACustomer.Gender = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.Gender, TestData);
+        }
+
+        [TestMethod]
+        public void HomeAddressPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            string TestData = "6 Cavendish Mews";
+            //assign data to the property
+            ACustomer.HomeAddress = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.HomeAddress, TestData);
+        }
+
+        [TestMethod]
+        public void LastNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            string TestData = "Doe";
+            //assign data to the property
+            ACustomer.LastName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.LastName, TestData);
+        }
+
+        [TestMethod]
+        public void PhoneNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test data to assign to the property
+            string TestData = "0123456789";
+            //assign data to the property
+            ACustomer.PhoneNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.PhoneNo, TestData);
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //test to see that the result if correct
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestCustomerIDFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the customername
+            if (ACustomer.CustomerID != 1)
+            {
+                //test to see that the result is correct
+                Assert.IsTrue(OK);
+            }               
+        }
+
+        [TestMethod]
+        public void TestDOBFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.DOB != Convert.ToDateTime("11/06/1989"))
+            {
+                OK = false;
+               
+            }
+             //test to see that the result is correct
+             Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestEmailAddressFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.EmailAddress != "igho@yahoo.com")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestFirstNameFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.FirstName != "igho")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestLastNameFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.LastName != "akponah")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestGenderFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.Gender != "male")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPhoneNoFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.PhoneNo != "0123456789")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestHomeAddressFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.HomeAddress != "6 Cavendish Mews")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestActiveFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            int CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+
+            if (ACustomer.Active != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string FirstName = "igho";
+            string LastName = "akponah";
+            string Gender = "male";
+            string HomeAddress = "6 Cavendish mews";
+            string EmailAddress = "igho@yahoo.com";
+            string PhoneNo = "0123456789";
+            string DOB = DateTime.Now.Date.ToString();
+            //invoke the method
+            Error = ACustomer.Valid(FirstName, LastName, Gender, HomeAddress, EmailAddress, PhoneNo, DOB);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void FirstNameMinLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean OK = false;
+            //create some test data to pass to the method
+            string FirstName = "igho"; //this should trigger an error
+            string LastName = "akponah";
+            string Gender = "male";
+            string HomeAddress = "6 Cavendish mews";
+            string EmailAddress = "igho@yahoo.com";
+            string PhoneNo = "0123456789";
+            string DOB = DateTime.Now.Date.ToString();
+            //invoke the method
+            OK = ACustomer.Valid(FirstName, LastName, Gender, HomeAddress, EmailAddress, PhoneNo, DOB);
+            //test to see that the result if correct
+            Assert.IsFalse(OK);
+        }
+    }
+}
