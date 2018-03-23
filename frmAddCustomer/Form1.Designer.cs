d7c655b4c72719a73312dcc8b7004cfc38de306c
ighoakponah <ighoakponah@IGHOAKPONAH7872>
2018-03-23 08:12:27 +0000

    ....

---
+++ frmAddCustomer/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace frmAddCustomer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.textBoxGender = new System.Windows.Forms.TextBox();
+            this.lblEmailAddress = new System.Windows.Forms.Label();
+            this.textBoxEmailAddress = new System.Windows.Forms.TextBox();
+            this.lblHomeAddress = new System.Windows.Forms.Label();
+            this.textBoxHomeAddress = new System.Windows.Forms.TextBox();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.textBoxDOB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.btnEditCustomer = new System.Windows.Forms.Button();
+            this.btnDelCustomer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(86, 60);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(119, 26);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(282, 55);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(198, 31);
+            this.textBoxFirstName.TabIndex = 1;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(86, 141);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(118, 26);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(282, 136);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(198, 31);
+            this.textBoxLastName.TabIndex = 3;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(86, 212);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(84, 26);
+            this.lblGender.TabIndex = 4;
+            this.lblGender.Text = "Gender";
+            // 
+            // textBoxGender
+            // 
+            this.textBoxGender.Location = new System.Drawing.Point(282, 212);
+            this.textBoxGender.Name = "textBoxGender";
+            this.textBoxGender.Size = new System.Drawing.Size(198, 31);
+            this.textBoxGender.TabIndex = 5;
+            // 
+            // lblEmailAddress
+            // 
+            this.lblEmailAddress.AutoSize = true;
+            this.lblEmailAddress.Location = new System.Drawing.Point(86, 281);
+            this.lblEmailAddress.Name = "lblEmailAddress";
+            this.lblEmailAddress.Size = new System.Drawing.Size(154, 26);
+            this.lblEmailAddress.TabIndex = 6;
+            this.lblEmailAddress.Text = "Email Address";
+            // 
+            // textBoxEmailAddress
+            // 
+            this.textBoxEmailAddress.Location = new System.Drawing.Point(282, 281);
+            this.textBoxEmailAddress.Name = "textBoxEmailAddress";
+            this.textBoxEmailAddress.Size = new System.Drawing.Size(198, 31);
+            this.textBoxEmailAddress.TabIndex = 7;
+            // 
+            // lblHomeAddress
+            // 
+            this.lblHomeAddress.AutoSize = true;
+            this.lblHomeAddress.Location = new System.Drawing.Point(86, 362);
+            this.lblHomeAddress.Name = "lblHomeAddress";
+            this.lblHomeAddress.Size = new System.Drawing.Size(157, 26);
+            this.lblHomeAddress.TabIndex = 8;
+            this.lblHomeAddress.Text = "Home Address";
+            // 
+            // textBoxHomeAddress
+            // 
+            this.textBoxHomeAddress.Location = new System.Drawing.Point(282, 362);
+            this.textBoxHomeAddress.Name = "textBoxHomeAddress";
+            this.textBoxHomeAddress.Size = new System.Drawing.Size(198, 31);
+            this.textBoxHomeAddress.TabIndex = 9;
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Location = new System.Drawing.Point(86, 438);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(138, 26);
+            this.lblDOB.TabIndex = 10;
+            this.lblDOB.Text = "Date Of Birth";
+            // 
+            // textBoxDOB
+            // 
+            this.textBoxDOB.Location = new System.Drawing.Point(282, 438);
+            this.textBoxDOB.Name = "textBoxDOB";
+            this.textBoxDOB.Size = new System.Drawing.Size(198, 31);
+            this.textBoxDOB.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 26);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Post Code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 516);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 31);
+            this.textBox1.TabIndex = 13;
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Location = new System.Drawing.Point(165, 604);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(97, 45);
+            this.btnAddCustomer.TabIndex = 14;
+            this.btnAddCustomer.Text = "Add";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            this.btnAddCustomer.Click += new System.EventHandler(this.btnAddCustomer_Click);
+            // 
+            // btnEditCustomer
+            // 
+            this.btnEditCustomer.Location = new System.Drawing.Point(348, 604);
+            this.btnEditCustomer.Name = "btnEditCustomer";
+            this.btnEditCustomer.Size = new System.Drawing.Size(98, 45);
+            this.btnEditCustomer.TabIndex = 15;
+            this.btnEditCustomer.Text = "Edit";
+            this.btnEditCustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnDelCustomer
+            // 
+            this.btnDelCustomer.Location = new System.Drawing.Point(496, 604);
+            this.btnDelCustomer.Name = "btnDelCustomer";
+            this.btnDelCustomer.Size = new System.Drawing.Size(97, 45);
+            this.btnDelCustomer.TabIndex = 16;
+            this.btnDelCustomer.Text = "Delete";
+            this.btnDelCustomer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 717);
+            this.Controls.Add(this.btnDelCustomer);
+            this.Controls.Add(this.btnEditCustomer);
+            this.Controls.Add(this.btnAddCustomer);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDOB);
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.textBoxHomeAddress);
+            this.Controls.Add(this.lblHomeAddress);
+            this.Controls.Add(this.textBoxEmailAddress);
+            this.Controls.Add(this.lblEmailAddress);
+            this.Controls.Add(this.textBoxGender);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox textBoxGender;
+        private System.Windows.Forms.Label lblEmailAddress;
+        private System.Windows.Forms.TextBox textBoxEmailAddress;
+        private System.Windows.Forms.Label lblHomeAddress;
+        private System.Windows.Forms.TextBox textBoxHomeAddress;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.TextBox textBoxDOB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddCustomer;
+        private System.Windows.Forms.Button btnEditCustomer;
+        private System.Windows.Forms.Button btnDelCustomer;
+    }
+}
+
